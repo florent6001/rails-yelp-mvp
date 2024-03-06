031004dc6b93fe29743244fdd677fefee2f785c0
Florent Vandroy <florentvandroy@gmail.com>
2024-03-06 15:31:15 +0100

    Change redirect/render when the model isn't saved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
   end
 
   private
